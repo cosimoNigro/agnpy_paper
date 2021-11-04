f56b07fc90d11b6167d7bb270f28d146dbaf964f
cosimoNigro <cosimonigro2@gmail.com>
2021-11-04 15:21:07 +0100

    trying to incorporate jetset execution in the scripts

--- figure_7_ssc_validation.py
+++ figure_7_ssc_validation.py
@@ -1,63 +1,99 @@
+# compare the synchrotron and SSC emission of agnpy against Dermer 2009 and jetset
 import pkg_resources
+import numpy as np
 import astropy.units as u
 from astropy.coordinates import Distance
 from agnpy.emission_regions import Blob
 from agnpy.synchrotron import Synchrotron
 from agnpy.compton import SynchrotronSelfCompton
+from jetset.jet_model import Jet
 from agnpy.utils.plot import load_mpl_rc, sed_x_label, sed_y_label
 import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 from pathlib import Path
-from utils import reproduce_sed
+from utils import reproduce_sed, time_function_call
 
-# emission region
+
+# agnpy emission region
 spectrum_norm = 1e48 * u.Unit("erg")
-pwl_dict = {
+spectrum_dict = {
     "type": "PowerLaw",
-    "parameters": {"p": 2.8, "gamma_min": 1e2, "gamma_max": 1e5},
+    "parameters": {"p": 2.8, "gamma_min": 1e2, "gamma_max": 1e7},
 }
-# blob parameters
 R_b = 1e16 * u.cm
 B = 1 * u.G
 z = Distance(1e27, unit=u.cm).z
 delta_D = 10
 Gamma = 10
-# define emission region and radiative process
-blob = Blob(R_b, z, delta_D, Gamma, B, spectrum_norm, pwl_dict)
+blob = Blob(R_b, z, delta_D, Gamma, B, spectrum_norm, spectrum_dict)
+# define the radiative process
 synch = Synchrotron(blob)
 ssc = SynchrotronSelfCompton(blob)
 
-# reference datasets
-data_synch_jetset = pkg_resources.resource_filename(
-    "agnpy", "data/reference_seds/jetset/data/synch_pwl_jetset_1.1.2.txt"
+
+# jetset emission region
+jet = Jet(
+    name="ssc",
+    electron_distribution="pl",
+    electron_distribution_log_values=False,
+    beaming_expr="bulk_theta",
 )
+jet.set_par("N", val=blob.n_e_tot.value)
+jet.set_par("p", val=blob.n_e.p)
+jet.set_par("gmin", val=blob.n_e.gamma_min)
+jet.set_par("gmax", val=blob.n_e.gamma_max)
+jet.set_par("R", val=blob.R_b.value)
+jet.set_par("B", val=blob.B.value)
+jet.set_par("BulkFactor", val=blob.Gamma)
+jet.set_par("theta", val=blob.theta_s.value)
+jet.set_par("z_cosm", val=blob.z)
+# remove SSA
+jet.spectral_components.Sync.state = "on"
+
+
+# reference datasets
 data_synch_dermer = pkg_resources.resource_filename(
     "agnpy",
     "data/reference_seds/dermer_menon_2009/figure_7_4/synchrotron_gamma_max_1e5.txt",
-)
-data_ssc_jetset = pkg_resources.resource_filename(
-    "agnpy", "data/reference_seds/jetset/data/ssc_pwl_jetset_1.1.2.txt"
 )
 data_ssc_dermer = pkg_resources.resource_filename(
     "agnpy", "data/reference_seds/dermer_menon_2009/figure_7_4/ssc_gamma_max_1e5.txt"
 )
 
-# synch SEDs
+# reproduce synch SED of Dermer 2009 with agnpy
+"""
 synch_nu_range = [1e9, 5e18] * u.Hz
-nu_synch_jetset, sed_synch_jetset, sed_synch_agnpy_jetset = reproduce_sed(
-    data_synch_jetset, synch, synch_nu_range
-)
 nu_synch_dermer, sed_synch_dermer, sed_synch_agnpy_dermer = reproduce_sed(
     data_synch_dermer, synch, synch_nu_range
 )
-# ssc SEDs
-ssc_nu_range = [1e14, 1e26] * u.Hz
-nu_ssc_jetset, sed_ssc_jetset, sed_ssc_agnpy_jetset = reproduce_sed(
-    data_ssc_jetset, ssc, ssc_nu_range
+"""
+# produce a synch SED with jetset
+nu_synch_jetset = np.logspace(9, 19, 100) * u.Hz
+jet.set_nu_grid(
+    nu_synch_jetset[0].value, nu_synch_jetset[-1].value, len(nu_synch_jetset)
 )
+jet.eval()
+nu_synch_jetset_ref = jet.spectral_components.Sync.SED.nu
+sed_synch_jetset_ref = jet.spectral_components.Sync.SED.nuFnu
+import IPython; IPython.embed()
+# compute agnpy SED on the same frequencies
+exit()
+"""
+# reproduce SSC SED of Dermer 2009 with agnpy
+ssc_nu_range = [1e14, 1e26] * u.Hz
 nu_ssc_dermer, sed_ssc_dermer, sed_ssc_agnpy_dermer = reproduce_sed(
     data_ssc_dermer, ssc, ssc_nu_range
 )
+"""
+# produce a SSC SED with jetset
+nu_ssc_jetset = np.logspace(14, 26, 100) * u.Hz
+jet.set_nu_grid(nu_ssc_jetset[0].value, nu_ssc_jetset[-1].value, len(nu_ssc_jetset))
+jet.eval()
+nu_ssc_jetset = jet.spectral_components.SSC.SED.nu
+sed_ssc_jetset = jet.spectral_components.SSC.SED.nuFnu
+# compute agnpy SED on the same frequencies
+sed_ssc_agnpy_jetset = ssc.sed_flux(nu_ssc_jetset)
+
 
 # figure
 load_mpl_rc()
@@ -69,6 +105,7 @@ ax1 = fig.add_subplot(spec[0, 0])
 ax2 = fig.add_subplot(spec[0, 1], sharey=ax1)
 ax3 = fig.add_subplot(spec[1, 0], sharex=ax1)
 ax4 = fig.add_subplot(spec[1, 1], sharex=ax2, sharey=ax3)
+"""
 # synch SEDs
 ax1.loglog(
     nu_synch_dermer,
@@ -85,6 +122,7 @@ ax1.loglog(
     color="k",
     label="Fig. 7.4, Dermer \& Menon (2009)",
 )
+"""
 ax1.loglog(
     nu_synch_jetset, sed_synch_jetset, ls="--", color="dodgerblue", label="jetset"
 )
@@ -93,6 +131,7 @@ ax1.legend(loc="best")
 ax1.set_title("synchrotron")
 ax1.set_ylim([1e-14, 1e-9])
 # ssc SEDs
+"""
 ax2.loglog(
     nu_ssc_dermer, sed_ssc_agnpy_dermer, lw=2.1, ls="-", color="crimson", label="agnpy"
 )
@@ -103,11 +142,12 @@ ax2.loglog(
     color="k",
     label="Fig. 7.4, Dermer \& Menon (2009)",
 )
+"""
 ax2.loglog(nu_ssc_jetset, sed_ssc_jetset, ls="--", color="dodgerblue", label="jetset")
 ax2.legend(loc="best")
 ax2.set_title("synchrotron self-Compton")
 # plot the deviation from the synchrotron reference in the bottom panel
-deviation_synch_dermer = sed_synch_agnpy_dermer / sed_synch_dermer - 1
+#eviation_synch_dermer = sed_synch_agnpy_dermer / sed_synch_dermer - 1
 deviation_synch_jetset = sed_synch_agnpy_jetset / sed_synch_jetset - 1
 ax3.grid(False)
 ax3.axhline(0, ls="-", color="darkgray")
@@ -117,6 +157,7 @@ ax3.axhline(0.3, ls=":", color="darkgray")
 ax3.axhline(-0.3, ls=":", color="darkgray")
 ax3.set_ylim([-0.5, 0.5])
 ax3.set_yticks([-0.4, -0.2, 0.0, 0.2, 0.4])
+"""
 ax3.semilogx(
     nu_synch_dermer,
     deviation_synch_dermer,
@@ -124,6 +165,7 @@ ax3.semilogx(
     color="k",
     label="Fig. 7.4, Dermer \& Menon (2009)",
 )
+"""
 ax3.semilogx(
     nu_synch_jetset,
     deviation_synch_jetset,
@@ -135,7 +177,7 @@ ax3.legend(loc="best", fontsize=10)
 ax3.set_xlabel(sed_x_label)
 ax3.set_ylabel(r"$\frac{\nu F_{\nu, \rm agnpy}}{\nu F_{\nu, \rm ref}} - 1$")
 # plot the deviation from the synchrotron reference in the bottom panel
-deviation_ssc_dermer = sed_ssc_agnpy_dermer / sed_ssc_dermer - 1
+#deviation_ssc_dermer = sed_ssc_agnpy_dermer / sed_ssc_dermer - 1
 deviation_ssc_jetset = sed_ssc_agnpy_jetset / sed_ssc_jetset - 1
 ax4.grid(False)
 ax4.axhline(0, ls="-", color="darkgray")
@@ -145,6 +187,7 @@ ax4.axhline(0.3, ls=":", color="darkgray")
 ax4.axhline(-0.3, ls=":", color="darkgray")
 ax4.set_ylim([-0.5, 0.5])
 ax4.set_yticks([-0.4, -0.2, 0.0, 0.2, 0.4])
+"""
 ax4.semilogx(
     nu_ssc_dermer,
     deviation_ssc_dermer,
@@ -153,11 +196,12 @@ ax4.semilogx(
     color="k",
     label="Fig. 7.4, Dermer \& Menon (2009)",
 )
+"""
 ax4.semilogx(
     nu_ssc_jetset, deviation_ssc_jetset, ls="--", color="dodgerblue", label="jetset",
 )
 ax4.legend(loc="best", fontsize=10)
 ax4.set_xlabel(sed_x_label)
 Path("figures").mkdir(exist_ok=True)
-fig.savefig(f"figures/figure_8.png")
-fig.savefig(f"figures/figure_8.pdf")
+fig.savefig(f"figures/figure_7.png")
+fig.savefig(f"figures/figure_7.pdf")
